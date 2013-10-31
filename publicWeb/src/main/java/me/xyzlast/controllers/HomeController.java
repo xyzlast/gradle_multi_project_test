7da0064b1ea92edf8a35282679690e0d2f05031c
xyzlast <xyzlast@gmail.com>
2013-10-31 17:14:22 +0900

    HomeController file update

--- publicWeb/src/main/java/me/xyzlast/controllers/HomeController.java
+++ publicWeb/src/main/java/me/xyzlast/controllers/HomeController.java
@@ -10,4 +10,20 @@ package me.xyzlast.controllers;
 public class HomeController {
     private int value;
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
