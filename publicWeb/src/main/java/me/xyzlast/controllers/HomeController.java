8c4b89310c888376aef8258515d384680ef15b1b
xyzlast <xyzlast@gmail.com>
2013-10-31 16:58:36 +0900

    upload sample files2

---
+++ publicWeb/src/main/java/me/xyzlast/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package me.xyzlast.controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ykyoon
+ * Date: 10/31/13
+ * Time: 4:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HomeController {
+    private int value;
+    private String name;
+}
